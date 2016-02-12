dbd4cdc3b784bb574da76ae242bd25b06136af9a
Kervin Pierre <kervin@sludev.com>
2016-02-12 00:02:24 +0000

    Adding controller, with next and cancel button stubs that work.

---
+++ src/logcheck-config/src/main/java/com/sludev/logs/logcheckConfig/controller/LogCheckConfigMainController.java
@@ -0,0 +1,32 @@
+package com.sludev.logs.logcheckConfig.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Created by kervin on 2016-02-11.
+ */
+public final class LogCheckConfigMainController
+{
+    private static final Logger LOGGER = LogManager.getLogger(LogCheckConfigMainController.class);
+
+    @FXML
+    Button buttonNext;
+
+    @FXML
+    Button buttonCancel;
+
+    @FXML
+    public void onButtonNextAction()
+    {
+        LOGGER.debug("Action for Button 'Next' pressed.");
+    }
+
+    @FXML
+    public void onButtonCancelAction()
+    {
+        LOGGER.debug("Action for Button 'Cancel' pressed.");
+    }
+}
